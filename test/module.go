2c59124edaaa99eb65dd3b711aca771615cc5e66
itsdalmo <kristian@doingit.no>
2019-09-13 08:59:25 +0200

    Add comments to exported functions

--- test/module.go
+++ test/module.go
@@ -7,16 +7,19 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 )
 
+// Expectations for the test suite.
 type Expectations struct {
 	NamePrefix string
 }
 
+// RunTestSuite for the module.
 func RunTestSuite(t *testing.T, region, namePrefix string, expected Expectations) {
 	if expected.NamePrefix != namePrefix {
 		t.Fatalf("expected: %s, got: %s", expected.NamePrefix, namePrefix)
 	}
 }
 
+// NewSession for AWS.
 func NewSession(t *testing.T, region string) *session.Session {
 	sess, err := session.NewSession(&aws.Config{
 		Region: aws.String(region),
